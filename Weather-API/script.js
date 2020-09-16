2a81f227e47c56d103e30f6d74f6be5f881ca36d
Alketa <alketa_1996@hotmail.com>
2020-09-17 02:03:01 +0300

    a simple weather api

--- Weather-API/script.js
+++ Weather-API/script.js
@@ -1,9 +1,12 @@
 //Because of jquery qe can use something like get json, that is going to get API's data because it 
 //is a json file
 
-const data = "";
+var city = "Washington DC";
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Indianapolis&appid=81edd230d41b142107dbd7724792cae2", data,
+const data = "";
+//http://api.openweathermap.org/data/2.5/weather?q=Indianapolis&appid=81edd230d41b142107dbd7724792cae2 is the other option
+
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=81edd230d41b142107dbd7724792cae2", data, 
     function (data, textStatus, jqXHR) {
         console.log(data);
 
@@ -11,6 +14,13 @@ $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Indianapolis&appid=8
         //getting the picture by writing the address and then specifying it in the json file
         //plus adding it on the png format
 
+        var temperature = Math.floor(data.main.temp);
+        var weather = data.weather[0].main;
+
         console.log(icon);
+
+        $('.icon').attr('src', icon); //the index html file has these class names so it shows them on the page
+        $('.temperature').append(temperature);
+        $('.weather').append(weather);
     }
 );
