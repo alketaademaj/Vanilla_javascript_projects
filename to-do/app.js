58217b71792ef7e737519d685216efb34b1e5183
Alketa <alketa_1996@hotmail.com>
2020-09-20 00:14:06 +0300

    adding process of the page

--- to-do/app.js
+++ to-do/app.js
@@ -18,6 +18,21 @@ function addTodo(event) {
     const newTodo = document.createElement("li");
     newTodo.innerText = "test";
     newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo); 
+    //check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class ="fas fa-check"></i>'; //adding this i-tag inside
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton); //appeding the button to the div, again as in above
+      //check track button
+    const trashdButton = document.createElement('button');
+    trashdButton.innerHTML = '<i class ="fas fa-trash"></i>'; //adding this i-tag inside
+    trashdButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton); //appeding the button to the div, again as in above
+      //now in the end, attach it to the ul in the html file (index)
+      //so append to list
+    todoList.appendChild(todoDiv);    
+
 
     console.log("is this working?");
 }
