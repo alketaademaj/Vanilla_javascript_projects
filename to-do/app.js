e03f82fa3c66f465b187113fc671547085d8baf8
Alketa <alketa_1996@hotmail.com>
2020-09-25 01:23:36 +0300

    to do

--- to-do/app.js
+++ to-do/app.js
@@ -2,7 +2,7 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-const filterOption = document.querySelector(".filter-todo");
+const filterOption = document.querySelector("#filter-todo");
 
 // Event listeners
 document.addEventListener("DOMContentLoaded", getTodos);
