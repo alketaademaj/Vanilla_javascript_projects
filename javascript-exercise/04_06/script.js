740a6837c849caa87634d0ac31ca33e90bff40b6
Alketa <alketa_1996@hotmail.com>
2020-11-01 23:49:54 +0200

    4-6

--- javascript-exercise/04_06/script.js
+++ javascript-exercise/04_06/script.js
@@ -4,4 +4,23 @@ var theBiggest = function(a,b) {
     return result;
 }
 
+//calling the variable name and then adding parenthesis after it,
+//pretending its a function and then passing arguments into that parenthesis
+//these arguments are then passed to the anonymous function inside
+//the variable, the function runs, some result is returned and then we're console logging the result
+//as if everything was a function
 console.log(theBiggest(7/9,13/25));
+console.log(theBiggest);
+
+var theBiggestt = (function(a,b) {
+    var result;
+    a>b ? result = ["a", a] : result = ["b", b];
+    return result;
+})(7/9,13/25)
+//envoke it by adding a set of parenthesis at the end
+//because of the aarguments below, we need to plase those arguments
+//inside this new set of parenthesis
+//console.log(theBiggestt(7/9,13/25));
+console.log(theBiggestt);
+
+//the browser runs the function when it is encountered
