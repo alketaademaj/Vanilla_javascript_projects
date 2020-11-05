607e3769f4c1e3db63a50c1e8e1876a28c447280
Alketa <alketa_1996@hotmail.com>
2020-11-05 23:50:17 +0200

    tic-tac-toe

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,15 +1,32 @@
 //selecting all the cells in the index file 
 const X_class = 'x'
 const Circle_class = 'circle'
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+
+]
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
 let circleTurn;
 
+startGame();
+
+function startGame() {
+    circleTurn = false;
 //loop
 //everytime we click on the cell we want to add
 cellElements.forEach(cell => {
     cell.addEventListener('click', handleClick, {once: true}) //once we click on the cell its not going to fire again
 })
+setBoardHoverClass()
+  }
 
 function handleClick(e) {
     const cell = e.target
