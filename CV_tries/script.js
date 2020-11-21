0924e22962687e6585ace76a756106e333f2a26e
Alketa <alketa_1996@hotmail.com>
2020-11-21 21:45:40 +0200

    cv

--- CV_tries/script.js
+++ CV_tries/script.js
@@ -47,11 +47,8 @@ function DarkLightMode() {
         }
         console.log(z)
     }
+
     factorial(3)
-
-
-
-
 
 //0,1,1,2,3,5,8,13,21,34
 
---
+++ Curriculum Vitae/Front-end/JS/Script.js
@@ -0,0 +1,4 @@
+function scrollDown(path){
+    var elem=document.getElementById(path);
+    elem.scrollIntoView();
+}
