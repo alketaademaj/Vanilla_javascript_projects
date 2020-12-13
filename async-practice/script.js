5dd158482be3443c37afb27b48f251f2980a2bb6
Alketa <alketa_1996@hotmail.com>
2020-12-13 21:36:31 +0200

    update

--- async-practice/script.js
+++ async-practice/script.js
@@ -1,32 +1,32 @@
 //callback exercise
 //--> create a post
-var tans = [
-  { title: "Post1", body: "This is body of post one" },
-  { title: "Post2", body: "This is body of post two" },
-];
-// definition part
-let postSend = (post, callback) => {
-  //arrow function
-  setTimeout(() => {
-    console.log("hello");
-    tans.push(post);
-  }, 10000);
-  callback();
-  //callback1({title:'Post4',body:'This is body of post four'});
-};
+// var tans = [
+//   { title: "Post1", body: "This is body of post one" },
+//   { title: "Post2", body: "This is body of post two" },
+// ];
+// // definition part
+// let postSend = (post, callback) => {
+//   //arrow function
+//   setTimeout(() => {
+//     console.log("hello");
+//     tans.push(post);
+//   }, 10000);
+//   callback();
+//   //callback1({title:'Post4',body:'This is body of post four'});
+// };
 
 // var callback1 = (postfour) => {
 //     tans.push(postfour)
 // }
 
-const getPost = () => {
-  setTimeout(() => {
-    console.log(tans);
-  }, 1000); //callback function
-};
+// const getPost = () => {
+//   setTimeout(() => {
+//     console.log(tans);
+//   }, 1000); //callback function
+// };
 
-// execution part
-postSend({ title: "Post3", body: "This is body of post three" }, getPost);
+// // execution part
+// postSend({ title: "Post3", body: "This is body of post three" }, getPost);
 // getPost()//you ar enow only supposed to this post I mean the post numbered 1
 
 //whenever we are in a class, the functions are called methods
@@ -49,38 +49,38 @@ postSend({ title: "Post3", body: "This is body of post three" }, getPost);
 //
 
 //defining the array
-var alketa = ["Apple", "Banana", "Pear", "Watermelon", "Strawberry"];
+// var alketa = ["Apple", "Banana", "Pear", "Watermelon", "Strawberry"];
 
-//defining the function
+// //defining the function
 
-function fruitbasket(berry, myname) {
-  setTimeout(() => {
-    alketa.push(berry);
-    console.log(myname);
-  }, 2000); // executing the function
-}
+// function fruitbasket(berry, myname) {
+//   setTimeout(() => {
+//     alketa.push(berry);
+//     console.log(myname);
+//   }, 2000); // executing the function
+// }
 
 // function fourthLargest() {
 // continue
 // }
 
-function getFruits1(fruits, berry1, fruity, tans) {
-  console.log("our program started !!!");
-  fruity(fruits, berry1, tans);
-}
-function getFruits(fruits, one, two) {
-  setTimeout(() => {
-    console.log(alketa);
-    fruits(one, two);
-  }, 3000);
-  setTimeout(() => {
-    console.log(alketa);
-  }, 3000);
-}
+// function getFruits1(fruits, berry1, fruity, tans) {
+//   console.log("our program started !!!");
+//   fruity(fruits, berry1, tans);
+// }
+// function getFruits(fruits, one, two) {
+//   setTimeout(() => {
+//     console.log(alketa);
+//     fruits(one, two);
+//   }, 3000);
+//   setTimeout(() => {
+//     console.log(alketa);
+//   }, 3000);
+// }
 
 //execute the function
 // fruitbasket(getFruits1, "Blueberry", getFruits); //instead of this
-getFruits1(fruitbasket, "Blueberry", getFruits, "Tans");
+// getFruits1(fruitbasket, "Blueberry", getFruits, "Tans");
 // our program started !!!
 // (wait for 3 sec)
 // [dsdfsfbd]
@@ -88,3 +88,32 @@ getFruits1(fruitbasket, "Blueberry", getFruits, "Tans");
 // push
 // (wait  for 3 sec)
 // [dfdssdfsdf]
+
+
+
+
+//------------------------------------------------------------
+
+function Name (theName) { //child 1
+    var message = `Welcome ${theName}, now you are part of functional javascript world.`; //template literals
+        return message;
+}
+
+function Geometric(theRounds) { //child 2
+
+}
+
+function Fibonacci(theCycle) { //child 3
+    var result = [];
+    for(var x=0; x<=theCycle; x++) {
+        
+    }
+    return result; 
+}
+
+function Execution(theChoice, theParam) { //parent
+    console.log(Name("Tanay"));
+    console.log(Fibonacci(6))
+}
+
+Execution()
